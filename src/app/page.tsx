a45c37031c301d6c0bd7e192580db7a6621ad281
natcasd <nathan_depiero@brown.edu>
2024-09-25 21:46:09 -0400

    fsdg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,19 @@ import profile from "./images/profile.png";
 export default function About() {
   return (
     <>
-      <div className="inset-0 -z-10 h-full w-full fixed inset-0 bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="flex flex-col sm:flex-row justify-center items-center h-screen p-4">
-          <div className="flex flex-col sm:flex-row justify-center items-center w-full space-y-2 sm:space-y-0 sm:space-x-2 overflow-y-auto">
-            <Image
-              src={profile}
-              alt="profile pic"
-              className="w-full h-full object-cover object-center rounded-lg"
-            />
-            <div className="flex flex-col w-full sm:w-2/3">
-              <p className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-black">
-                Hi! I&apos;m Nathan.
+      <div className="-z-10 absolute w-full h-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+        <div className="flex flex-col sm:flex-row justify-center items-center min-h-screen p-4 overflow-auto">
+          <div className="flex flex-col sm:flex-row justify-center items-center w-full max-w-screen-lg space-y-2 sm:space-y-0 sm:space-x-4">
+            <div className="w-full sm:w-auto flex justify-center">
+              <Image
+                src={profile}
+                alt="profile pic"
+                className="w-full h-auto max-w-md object-cover object-center rounded-lg"
+              />
+            </div>
+            <div className="flex flex-col w-full sm:w-auto sm:max-w-lg text-left">
+              <p className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-7xl xl:text-7xl text-black">
+                Hi! I&apos;m <u>Nathan</u>.
               </p>
               <p className="text-sm sm:text-base md:text-lg">
                 I am a Master&apos;s student at Brown University studying
